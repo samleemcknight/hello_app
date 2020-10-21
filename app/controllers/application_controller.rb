0acfb8b238ea846e89ec44816c4e29426761c7b2
Sam McKnight <samleemcknight@gmail.com>
2020-10-21 09:58:11 +0100

    Added a greeting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
 
   def hello
-    render html: "¡Hola, mundo! 안녕하세요!"
+    render html: "¡Hola, mundo! 안녕하세요! What's up, doc?"
   end
 
   def goodbye
     render html: "Goodbye, world!"
-  end 
+  end
 end
