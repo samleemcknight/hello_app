ff6ffd7b38251df461432c72357b77d438050a7b
Sam McKnight <samleemcknight@gmail.com>
2020-10-21 10:22:58 +0100

    Added a line

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 
   def hello
-    render html: "¡Hola, mundo! 안녕하세요! What's up, doc?"
+    render html: "¡Hola, mundo! 안녕하세요! What's up, doc? Wie geht's dir?"
   end
 
   def goodbye
